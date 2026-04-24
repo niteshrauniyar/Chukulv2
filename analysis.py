77684b643ed0d8949f76a687b434e09ea0181085
Nitesh Kumar Gupta <104880718+niteshrauniyar@users.noreply.github.com>
2026-04-24 15:21:34 +0545

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -6,9 +6,11 @@ def analyze_market(df):
         return df
 
     df = df.copy()
+
     df["return_pct"] = ((df["ltp"] - df["open"]) / df["open"]) * 100
     df["turnover_m"] = df["turnover"] / 1000000
     df["turnover_m"] = df["turnover_m"].replace(0, 0.0001)
+
     df["amihud"] = abs(df["return_pct"]) / df["turnover_m"]
 
     high_turn = df["turnover"].quantile(0.8)
@@ -17,6 +19,10 @@ def analyze_market(df):
 
     df["cluster_name"] = "Retail"
     df.loc[df["amihud"] >= high_amihud, "cluster_name"] = "Speculative"
-    df.loc[(df["turnover"] >= high_turn) & (df["amihud"] <= low_amihud), "cluster_name"] = "Institutional"
+    df.loc[
+        (df["turnover"] >= high_turn) &
+        (df["amihud"] <= low_amihud),
+        "cluster_name"
+    ] = "Institutional"
 
     return df
